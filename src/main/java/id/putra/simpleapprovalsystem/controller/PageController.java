3f1703a2cdee4131374c98787530c418d0e1a86d
putra4648 <wakwaujaim@gmail.com>
2025-01-28 20:12:20 +0700

    ✏️ Fix: wrong commit information
    should be this not there

--- src/main/java/id/putra/simpleapprovalsystem/controller/PageController.java
+++ src/main/java/id/putra/simpleapprovalsystem/controller/PageController.java
@@ -1,4 +1,14 @@
 package id.putra.simpleapprovalsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class PageController {
+    @GetMapping("")
+    public ModelAndView index(ModelMap model) {
+        return new ModelAndView("index", model);
+    }
 }
