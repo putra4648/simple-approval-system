577b54c5241c955d88ba3c91aaaebee66f4f7fbd
putra4648 <wakwaujaim@gmail.com>
2025-01-28 19:45:20 +0700

    🎉 Initial commit

---
+++ src/main/java/id/putra/simpleapprovalsystem/SimpleApprovalSystemApplication.java
@@ -0,0 +1,13 @@
+package id.putra.simpleapprovalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApprovalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApprovalSystemApplication.class, args);
+    }
+
+}
