c8fdaddbc134e2395acff6c04cb4082647fe3208
putra4648 <wakwaujaim@gmail.com>
2025-01-29 13:49:13 +0700

    🚩 Feat: UI Controller and Item Controller

--- src/main/java/id/putra/simpleapprovalsystem/controller/PageController.java
+++ src/main/java/id/putra/simpleapprovalsystem/controller/PageController.java
@@ -1,4 +1,22 @@
 package id.putra.simpleapprovalsystem.controller;
 
+import id.putra.simpleapprovalsystem.request.ItemRequest;
+import id.putra.simpleapprovalsystem.service.ApprovalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class PageController {
+    @Autowired
+    private ApprovalService approvalService;
+
+    @GetMapping("")
+    public ModelAndView index(ModelMap model) {
+        model.put("approvals", approvalService.getApprovals());
+        model.put("item", new ItemRequest());
+        return new ModelAndView("index", model);
+    }
 }
